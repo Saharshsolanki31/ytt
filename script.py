0aaa814fc3da39bb2cf9e51f3b09761873bef56e
paytmbadshah <57286263+paytmbadshah@users.noreply.github.com>
2021-06-09 19:26:28 +0530

    Update script.py

--- script.py
+++ script.py
@@ -83,13 +83,13 @@ def process_insta_step(message):
         user = user_dict[chat_id]
         msgs=bot.send_message(chat_id, " Task : Loading Browser !!")
         try:
-            chrome_options = webdriver.ChromeOptions()
-            chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
-            chrome_options.add_argument("--headless")
-            chrome_options.add_argument("--disable-dev-shm-usage")
-            chrome_options.add_argument("--no-sandbox")
-            browser = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), options=chrome_options)
-            # browser = webdriver.Chrome(executable_path=r"C:\Chrome_driver\chromedriver.exe")
+            # chrome_options = webdriver.ChromeOptions()
+            # chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+            # chrome_options.add_argument("--headless")
+            # chrome_options.add_argument("--disable-dev-shm-usage")
+            # chrome_options.add_argument("--no-sandbox")
+            # browser = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), options=chrome_options)
+            browser = webdriver.Chrome(executable_path=r"C:\Chrome_driver\chromedriver.exe")
         except:
             bot.send_message(chat_id,"Task Failed : I Got An error While Loading Browerser")
             # browser.close()
@@ -98,9 +98,9 @@ def process_insta_step(message):
         # try:
         bot.edit_message_text(chat_id=chat_id, text="Task : Browser Loaded Trying To Login", message_id=msgs.message_id)
         browser.get("https://www.instagram.com/")
-        bot.edit_message_text(chat_id=chat_id, text="Task : Website Loaded ", message_id=msgs.message_id)
         time.sleep(20)
         username_input = browser.find_element_by_xpath("//*[@id='loginForm']/div/div[1]/div/label/input").send_keys(user.username)
+        bot.edit_message_text(chat_id=chat_id, text="Entered Username", message_id=msgs.message_id)
         password_input = browser.find_element_by_xpath("//*[@id='loginForm']/div/div[2]/div/label/input").send_keys(user.password)
         bot.edit_message_text(chat_id=chat_id, text="Entered Login Details", message_id=msgs.message_id)
         time.sleep(3)
