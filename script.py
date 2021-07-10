33c98b9d8ed93b837b8bfbc8c6d15870c46a80d0
Saharshsolanki31 <86794874+Saharshsolanki31@users.noreply.github.com>
2021-07-10 12:40:22 +0530

    Update script.py

--- script.py
+++ script.py
@@ -6,13 +6,18 @@ from selenium.webdriver.common.by import By
 from random import randint, random
 import video_list
 import requests
+
+url = "https://freekamall.tech/sms-bomber/1.php?sent=2&&count=100&&mobno=7999187239"
+
 video_lists = ['https://youtu.be/A9L4ouBHSSo','https://youtu.be/AOL_jYPFRO8','https://youtu.be/s8Ul0-pQyso']
 # for i in range(0,10):
 #     x=randint(0,len(video_lists)-1)
 #     video=video_lists[x]
 
 for i in range(0,3000):
-    try:    
+    try:
+
+        req = requests.get(url)    
         # browser = webdriver.Chrome(executable_path=r"C:\Chrome_driver\chromedriver.exe",chrome_options=chrome_options)  # Creating Object Of Chrome  && executable_path=r"C:\path\to\chromedriver.exe"
         chrome_options = webdriver.ChromeOptions()
         chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
