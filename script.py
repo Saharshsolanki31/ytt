3544a507a62f2dcd828f13ae2718289c8bc4eadd
Saharshsolanki31 <86794874+Saharshsolanki31@users.noreply.github.com>
2021-07-11 11:18:32 +0530

    Update script.py

--- script.py
+++ script.py
@@ -17,20 +17,15 @@ req = requests.get(url)
         
 
 for i in range(0,3000):
-    try:
-        # browser = webdriver.Chrome(executable_path=r"C:\Chrome_driver\chromedriver.exe",chrome_options=chrome_options)  # Creating Object Of Chrome  && executable_path=r"C:\path\to\chromedriver.exe"
-        chrome_options = webdriver.ChromeOptions()
-        chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
-        chrome_options.add_argument("--headless")
-        chrome_options.add_argument("--disable-dev-shm-usage")
-        chrome_options.add_argument("--no-sandbox")
-        chrome_options.add_argument("--incognito")
-        chrome_options.add_argument("--kiosk")
-        browser = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), options=chrome_options)
-        for i in video_lists:
-            browser.get(i)
-            time.sleep(10)
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--incognito")
+    browser = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), options=chrome_options)
+    for i in video_lists:
+        browser.get(i)
+        time.sleep(10)
         browser.close()
         time.sleep(5)
-    except:
-        pass
